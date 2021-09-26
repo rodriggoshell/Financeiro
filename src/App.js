c982721ac7189d2e957d9589d72fc7386ced99e6
rodriggoshell <rodriggoshell@gmail.com>
2021-09-25 22:21:31 -0300

    criando as rotas(Router)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bem-Vindo</h1>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/sobre">
+          <Sobre />
+        </Route>
+        <Route path="/contatos">
+          <Contatos />
+        </Route>
+        <Route path="newproject">
+          <NovosProjetos />
+        </Route>
+      </Switch>
+      <p>Footer Aqui</p>
+    </Router>
   );
 }
 
